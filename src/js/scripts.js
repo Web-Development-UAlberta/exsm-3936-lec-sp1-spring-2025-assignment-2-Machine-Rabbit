b71b978f6de3a2624774f50e68e4da6558d1cc71
Machine-Rabbit <machine.rabbit.media@gmail.com>
2025-04-26 18:17:46 -0600

    Added Abstract Class and 3 Child Classes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,97 @@
-// eslint-disable-next-line no-unused-vars
-/* global output, input */
-// eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  class AbstractShape {
+    constructor(color, perimeter, area){
+      if(new.target===AbstractShape){
+        throw new Error ("Can't instantiage Abstract Class Abstract Shape");
+      }
+      color = "string";
+      this.perimeter = perimeter;
+      this.area = area;
+    }
   }
+  class Rectangle extends AbstractShape{
+    constructor(length, width){
+      super(color, perimeter, area);
+      this.length = length;
+      this.width = width;
+      this.perimeter = length + length + width + width;
+      this.area = length * width;
+    }
+    get length(){
+      return this._length;
+    }
+    get width(){
+      return this._width; 
+    }
+    set length(value){
+      this._length = value;
+    }
+    set width(value){
+      this._width = value;
+    }
+  }
+  class Triangle extends AbstractShape{
+    constructor(base, height){
+      super(color, perimeter, area);
+      this.base = base;
+      this.height = height;
+      this.perimeter = base + base + base;
+      this.area = (base*height)/2;
+    }
+    get base() {
+      return this._base;
+    }
+    get height(){
+      return this._height;
+    }
+    set base(value){
+      this._base = value;
+    }
+    set height(value){
+      this._height = value;
+    }
+  }
+  class Circle extends AbstractShape{ //ADD CIRCUMFERENCE ALIAS
+    constructor(radius){
+      super(color, perimeter, area);
+      this.radius = radius;
+      this.diameter = radius + radius;
+      this.perimeter = 3.14 *this.diameter;
+      this.area = 3.14*(radius*radius);
+    }
+    get radius (){
+      return this._radius;
+    }
+    get diameter(){
+      return this._diameter;
+    }
+    set radius (value){
+      this._radius = value;
+    }
+    set diameter(value){
+      this._diameter = value;
+    }
+  }
+  //... ADD HEXAGON 
+  let color;
+  let perimeter;
+  let area;
+  let myShape = new Rectangle(10, 10);
+  let myShape1 = new Triangle(10, 10);
+  let myShape2 = new Circle(8);
+  output(`Testing rectangle perimeter: ${myShape.perimeter} units.`);
+  output(`Testing rectangle area: ${myShape.area} units.`);
+  output(`Testing rectangle length: ${myShape.length} units.`);
+  output(`Testing rectangle width: ${myShape.width} units.`);
+  //...
+  output(`Testing triangle perimeter: ${myShape1.perimeter} units.`);
+  output(`Testing triangle area: ${myShape1.area} units.`);
+  output(`Testing triangle base: ${myShape1.base} units.`);
+  output(`Testing triangle height: ${myShape1.height} units.`);
+  //...
+  output(`Testing circle perimeter: ${myShape2.perimeter} units.`);
+  output(`Testing circle area: ${myShape2.area} units.`);
+  output(`Testing circle radius: ${myShape2.radius} units.`);
+  output(`Testing circle diameter: ${myShape2.diameter} units.`);
+  output(`Testing circle circumference: ${myShape2.circumference} units.`);
 }
